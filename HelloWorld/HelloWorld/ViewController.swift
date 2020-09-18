a0e0da98df2ef6e66d8b03696d21892f63cc39df
강현석 <kekemusa37@nate.com>
2020-09-18 21:58:44 +0900

    HelloWorld : [추가] 버튼 이벤트

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lbHello: UILabel!
+    @IBOutlet var txtName: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func send(_ sender: UIButton) {
+        lbHello.text = "Hello, " + txtName.text!
+    }
+    
 }
 
