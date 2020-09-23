361fefe1206a920fd76a36e35a18a0fc92bc62b3
강현석 <kekemusa37@nate.com>
2020-09-23 23:53:26 +0900

    ImageView : [추가] IBOutlet, IBAction

--- ImageView/ImageView/ViewController.swift
+++ ImageView/ImageView/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var btnResize: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func resizeImage(_ sender: Any) {
+    }
+    
+    @IBAction func showImage(_ sender: Any) {
+    }
 }
 
