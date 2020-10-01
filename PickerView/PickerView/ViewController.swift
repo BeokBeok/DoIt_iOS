5748f489e80ffa387104eea3ee383a82f50f8d03
강현석 <kekemusa37@nate.com>
2020-10-01 21:26:48 +0900

    PickerView : [구현] 선택한 이미지 이름과 해당 이미지 출력

--- PickerView/PickerView/ViewController.swift
+++ PickerView/PickerView/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController
 : UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     let MAX_ARRAY_NUM = 10
     let PICKER_VIEW_COLUMN = 1
+    var imageArray = [UIImage?]()
     var imageFileName = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg",
                          "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg"]
     
@@ -20,6 +21,14 @@ class ViewController
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        for i in 0..<MAX_ARRAY_NUM {
+            let image = UIImage(named: imageFileName[i])
+            imageArray.append(image)
+        }
+        
+        lbImageFileName.text = imageFileName[0]
+        imageView.image = imageArray[0]
     }
 
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -33,5 +42,9 @@ class ViewController
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return imageFileName[row]
     }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        lbImageFileName.text = imageFileName[row]
+        imageView.image = imageArray[row]
+    }
 }
-
