dfb34dbd1f4535e880a9532824b92446149d64a6
강현석 <kekemusa37@nate.com>
2020-10-02 21:16:04 +0900

    Alert : [구현] 전구 켜기

--- Alert/Alert/ViewController.swift
+++ Alert/Alert/ViewController.swift
@@ -9,14 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let imgOn = UIImage(named: "lamp-on.png")
+    let imgOff = UIImage(named: "lamp-off.png")
+    let imgRemove = UIImage(named: "lamp-remove.png")
+    
+    var isLampOn = true
+    
     @IBOutlet var ivLamp: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        ivLamp.image = imgOn
     }
 
     @IBAction func onLamp(_ sender: UIButton) {
+        if isLampOn == true {
+            let lampOnAlert = UIAlertController(
+                title: "경고",
+                message: "현재 On 상태입니다.",
+                preferredStyle: UIAlertController.Style.alert
+            )
+            let onAction = UIAlertAction(
+                title: "네 알겠습니다.",
+                style: UIAlertAction.Style.default,
+                handler: nil
+            )
+            lampOnAlert.addAction(onAction)
+            present(lampOnAlert, animated: true, completion: nil)
+        } else {
+            ivLamp.image = imgOn
+            isLampOn = true
+        }
     }
     
     @IBAction func offLamp(_ sender: UIButton) {
