6a9b4e9f774f804340988a676be15df8d16b70c9
강현석 <kekemusa37@nate.com>
2020-10-02 22:05:25 +0900

    WebView : [추가] Outlet 및 Action

--- WebView/WebView/ViewController.swift
+++ WebView/WebView/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var tfUrl: UITextField!
+    @IBOutlet var wv: WKWebView!
+    @IBOutlet var aiv: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func gotoUrl(_ sender: UIButton) {
+    }
+    
+    @IBAction func gotoSite1(_ sender: UIButton) {
+    }
+    
+    @IBAction func gotoSite2(_ sender: UIButton) {
+    }
+    
+    @IBAction func loadHtml(_ sender: UIButton) {
+    }
+    
+    @IBAction func loadFile(_ sender: UIButton) {
+    }
+    
+    @IBAction func stop(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func reload(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func goBack(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func goForward(_ sender: UIBarButtonItem) {
+    }
+    
 }
 
