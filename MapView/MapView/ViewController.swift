0804792d813a4d9b9fc5c8950693311913aac2c1
강현석 <kekemusa37@nate.com>
2020-10-07 20:47:04 +0900

    MapView : [추가] Outlet 및 Action

--- MapView/MapView/ViewController.swift
+++ MapView/MapView/ViewController.swift
@@ -6,14 +6,21 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mapView: MKMapView!
+    @IBOutlet var lbLocationInfo: UILabel!
+    @IBOutlet var lbLocationDetail: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeLocation(_ sender: UISegmentedControl) {
+    }
+    
 }
 
