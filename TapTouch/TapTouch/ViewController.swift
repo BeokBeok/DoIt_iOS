606885284174a27c5734e1a1df8a4c745b619365
강현석 <kekemusa37@nate.com>
2020-10-15 23:00:44 +0900

    TapTouch : [추가] Outlet 변수

--- TapTouch/TapTouch/ViewController.swift
+++ TapTouch/TapTouch/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lbMessage: UILabel!
+    @IBOutlet var lbTapCount: UILabel!
+    @IBOutlet var lbTouchCount: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
