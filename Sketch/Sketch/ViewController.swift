517a49eecbdba7c9dab223700ff0b9cb99e10b1c
강현석 <kekemusa37@nate.com>
2020-10-15 23:26:00 +0900

    Sketch : [구현] 드로잉 기능

--- Sketch/Sketch/ViewController.swift
+++ Sketch/Sketch/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var lastPoint: CGPoint!
+    var lineSize: CGFloat = 2.0
+    var lineColor = UIColor.red.cgColor
+    
     @IBOutlet var iv: UIImageView!
     
     override func viewDidLoad() {
@@ -17,7 +21,56 @@ class ViewController: UIViewController {
     }
 
     @IBAction func clear(_ sender: UIButton) {
+        iv.image = nil
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch
+        
+        lastPoint = touch.location(in: iv)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        UIGraphicsBeginImageContext(iv.frame.size)
+        UIGraphicsGetCurrentContext()?.setStrokeColor(lineColor)
+        UIGraphicsGetCurrentContext()?.setLineCap(CGLineCap.round)
+        UIGraphicsGetCurrentContext()?.setLineWidth(lineSize)
+        
+        let touch = touches.first! as UITouch
+        let currentPoint = touch.location(in: iv)
+        
+        iv.image?.draw(in: CGRect(x: 0, y: 0, width: iv.frame.size.width, height: iv.frame.size.height))
+        
+        UIGraphicsGetCurrentContext()?.move(to: CGPoint(x: lastPoint.x, y: lastPoint.y))
+        UIGraphicsGetCurrentContext()?.addLine(to: CGPoint(x: currentPoint.x, y: currentPoint.y))
+        UIGraphicsGetCurrentContext()?.strokePath()
+        
+        iv.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        lastPoint = currentPoint
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        UIGraphicsBeginImageContext(iv.frame.size)
+        UIGraphicsGetCurrentContext()?.setStrokeColor(lineColor)
+        UIGraphicsGetCurrentContext()?.setLineCap(CGLineCap.round)
+        UIGraphicsGetCurrentContext()?.setLineWidth(lineSize)
+        
+        iv.image?.draw(in: CGRect(x: 0, y: 0, width: iv.frame.size.width, height: iv.frame.size.height))
+        
+        UIGraphicsGetCurrentContext()?.move(to: CGPoint(x: lastPoint.x, y: lastPoint.y))
+        UIGraphicsGetCurrentContext()?.addLine(to: CGPoint(x: lastPoint.x, y: lastPoint.y))
+        UIGraphicsGetCurrentContext()?.strokePath()
+        
+        iv.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            iv.image = nil
+        }
+    }
 }
 
