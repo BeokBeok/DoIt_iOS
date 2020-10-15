255e9fc7e4052fbe809b35906cb4c95a683df516
강현석 <kekemusa37@nate.com>
2020-10-15 23:08:00 +0900

    TapTouch : [구현] 터치 이벤트

--- TapTouch/TapTouch/ViewController.swift
+++ TapTouch/TapTouch/ViewController.swift
@@ -18,6 +18,28 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch
+        
+        lbMessage.text = "Touches Began"
+        lbTapCount.text = String(touch.tapCount)
+        lbTouchCount.text = String(touches.count)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch
+        
+        lbMessage.text = "Touches Moved"
+        lbTapCount.text = String(touch.tapCount)
+        lbTouchCount.text = String(touches.count)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch
+        
+        lbMessage.text = "Touches Ended"
+        lbTapCount.text = String(touch.tapCount)
+        lbTouchCount.text = String(touches.count)
+    }
 }
 
