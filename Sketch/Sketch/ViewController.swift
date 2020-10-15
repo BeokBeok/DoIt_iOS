e652fede08bf8a82492bb51409bdb5daf7e69374
강현석 <kekemusa37@nate.com>
2020-10-15 23:15:05 +0900

    Sketch : [추가] Outlet 변수 및 Action

--- Sketch/Sketch/ViewController.swift
+++ Sketch/Sketch/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var iv: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func clear(_ sender: UIButton) {
+    }
+    
 }
 
