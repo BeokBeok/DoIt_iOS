5a2db507891d6ffdbd82e2ef5758789baa040455
강현석 <kekemusa37@nate.com>
2020-10-16 19:30:44 +0900

    SwipeGesture : [추가] Outlet 변수

--- SwipeGesture/SwipeGesture/ViewController.swift
+++ SwipeGesture/SwipeGesture/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imgUp: UIImageView!
+    @IBOutlet var imgDown: UIImageView!
+    @IBOutlet var imgLeft: UIImageView!
+    @IBOutlet var imgRight: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
