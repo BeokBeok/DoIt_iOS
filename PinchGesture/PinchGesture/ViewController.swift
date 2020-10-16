94cd7558aa5702807c70a5327589ddc966319872
강현석 <kekemusa37@nate.com>
2020-10-16 21:03:16 +0900

    PinchGesture : [구현] 텍스트 핀치

--- PinchGesture/PinchGesture/ViewController.swift
+++ PinchGesture/PinchGesture/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lbPinch: UILabel!
+    
+    var initialFontSize: CGFloat!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let pinch = UIPinchGestureRecognizer(target: self, action: #selector(ViewController.doPinch(_:)))
+        self.view.addGestureRecognizer(pinch)
     }
 
-
+    @objc func doPinch(_ pinch: UIPinchGestureRecognizer) {
+        if (pinch.state == UIGestureRecognizer.State.began) {
+            initialFontSize = lbPinch.font.pointSize
+        } else {
+            lbPinch.font = lbPinch.font.withSize(initialFontSize * pinch.scale)
+        }
+    }
 }
 
